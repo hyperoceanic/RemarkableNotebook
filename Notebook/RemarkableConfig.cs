eedaf4e720871b1156a60a28c7e590dc03f91065
Mark Smith <hyperoceanic@users.noreply.github.com>
2025-06-18 10:06:18 +0100

    Add record to encaspulate device capabilities

--- Notebook/RemarkableConfig.cs
+++ Notebook/RemarkableConfig.cs
@@ -2,19 +2,39 @@ namespace PDFNote.Remarkable;
 
 using PDFNote.Model;
 
+public record Device(DeviceSKU SKU, Orientation Orientation, Handedness Handedness)
+{
+    public int Height { get => DeviceUtils.GetDeviceDimensions(SKU, Orientation).Height; }
+    public int Width {get => DeviceUtils.GetDeviceDimensions(SKU, Orientation).Width; }
+}
+
+
+/// <summary>
+/// The model of device to generate the PDF for.
+/// </summary>
 public enum DeviceSKU
 {
     Remarkable2,
     RemarkablePaperPro
 }
 
+/// <summary>
+/// Whether to render the documnent margin items like page numbers for left or right-handed users.
+/// </summary>
+public enum Handedness
+{
+    Left,
+    Right
+}
+
+
 public static class DeviceUtils
 {
     /// <summary>
-    /// returns pixels.
+    /// Returns dimension of device screen in pixels.
     /// </summary>
-    /// <param name="sku"></param>
-    /// <param name="orientation"></param>
+    /// <param name="sku">Model of device to generate PDF For</param>
+    /// <param name="orientation">Portrait or Landscape</param>
     /// <returns></returns>
     public static PageDimensions GetDeviceDimensions(DeviceSKU sku, PDFNote.Model.Orientation orientation) =>
     (sku, orientation) switch
